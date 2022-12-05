52f683043104b605788c8b31e6a232c745bb0262
ashirafkyaze <kyazzeashiraf91@gmail.com>
2022-12-05 04:51:55 -0800

    0-print_list_integer.py

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,4 @@
-#!/usr/bin/pyhton3
-def print_list_integer(my_list = []):
+#!/usr/bin/python3
+def print_list_integer(my_list=[]):
     for arr in range(len(my_list)):
         print("{:d}".format(my_list[arr]))
